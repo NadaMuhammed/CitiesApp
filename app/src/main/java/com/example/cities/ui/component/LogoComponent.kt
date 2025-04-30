c6c2f2872b4d5fe9fdbdb7856a26b619f6c4e376
Nada Muhammed <Nada.Mohamed@mdi.com.eg>
2025-04-30 12:17:35 +0300

    Changed Font.

--- app/src/main/java/com/example/cities/ui/component/LogoComponent.kt
+++ app/src/main/java/com/example/cities/ui/component/LogoComponent.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.cities.domain.model.City
 
@@ -26,7 +27,8 @@ fun LogoComponent(
     ) {
         Text(
             text = city.country ?: "",
-            color = Color.White
+            color = Color.White,
+            fontWeight = FontWeight.Bold
         )
     }
 }
